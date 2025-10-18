915ede304815f11b75eddd83a6da36a6c3bf66d7
Idan Ariav <idanaok@proton.me>
2025-10-18 07:04:47 +0300

    update PeriodDays function

--- Extras/Snippets/getPeriodDays.js
+++ Extras/Snippets/getPeriodDays.js
@@ -1,13 +1,19 @@
-function getPeriodDays(date, period='week') {
+function getPeriodDays(date, period='week', format='yaml') {
   const startDate = moment(date, 'YYYY-MM-DD').startOf(period);
   const endDate = moment(date, 'YYYY-MM-DD').endOf(period);
   const daysCount = endDate.diff(startDate, 'days') + 1;
-
-  const allDayDates = Array.from({ length: daysCount }, (_, i) =>
+  if (format === "yaml") {
+      const allDayDates = Array.from({ length: daysCount }, (_, i) =>
     `- "[[${startDate.clone().add(i, 'days').format('YYYY-MM-DD')}]]"`
   ).join('\n');
-
-  return allDayDates;
+    return allDayDates;
+}
+  else {
+    const allDayDates = Array.from({ length: daysCount }, (_, i) =>
+    `[[${startDate.clone().add(i, 'days').format('YYYY-MM-DD')}]]`
+  ).join(' ');
+    return allDayDates;
+  }
 }
 
 module.exports = getPeriodDays;
